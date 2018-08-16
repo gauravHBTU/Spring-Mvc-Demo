982165c36d8c84a7f77ddbe007311605e3770f78
cappygaurav <gaurav@priceeagle.in>
2018-08-16 18:03:44 +0530

    demo1

---
+++ MvcDemo1/src/main/java/com/mvcdemo/util/Message.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mvcdemo.util;
+
+public class Message {
+    
+    protected String title;
+    protected String text;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" + "title=" + title + ", text=" + text + '}';
+    }
+    
+    
+}
